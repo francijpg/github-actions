2461417907fa9d52b083c911e572327b6364eac7
Francisco PG <hellofrancijpg@gmail.com>
2023-02-04 14:50:52 -0300

    chore: first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+
+// console.log("Hello, World!");
+require('@francijpg/github-actions-package')
